a0f5cf91c19f521c505e189b2fd8d7d6f37ba2d7
«Denis» <tytdenis2@gmail.com>
2022-08-23 16:54:54 +0500

    add Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите 3-х значное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int secondDigit =  num / 10 % 10; 
+
+System.Console.WriteLine($"{num} → {secondDigit}");
