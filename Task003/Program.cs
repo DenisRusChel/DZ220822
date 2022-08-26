028c8052552ec571c97c74c698ce64222c9192f2
«Denis» <tytdenis2@gmail.com>
2022-08-26 14:45:37 +0500

    add changes Task003

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -9,15 +9,16 @@
 System.Console.WriteLine("Введите число");
 int num = int.Parse(Console.ReadLine());
 
-if (num < 1 || num> 7)
-{
-    System.Console.WriteLine("Введите число от 1 до 7");
-}
-if (num == 5 ||num == 6 )
-{
-    System.Console.WriteLine($"{num} → да");
-}
-else
-{
-    System.Console.WriteLine($"{num} → нет");
-}
+    if (num < 1 || num> 7)
+    {
+        System.Console.WriteLine("Введите число от 1 до 7");
+    }
+    if (num == 6 ||num == 7 )
+    {
+        System.Console.WriteLine($"{num} → да");
+    }
+
+    else
+    {
+        System.Console.WriteLine($"{num} → нет");
+    }
